96808497538af2dc6573389116cdef9be80f78e9
Coulibaly Ngolo Issa <coulibalyissa956@gmail.com>
2026-03-08 01:39:15 +0000

    create Mutualist admin

--- smsapp/admin.py
+++ smsapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Mutualist
 
-# Register your models here.
+@admin.register(Mutualist)
+class Mutualist(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "phone_number"]
+    list_filter = ["first_name", "last_name"]
+    search_fields = ["first_name", "last_name", "phone_number"]
+    ordering = ("last_name",)
