b2bb76042aaf2d5346576f02d7e50cf403462d4f
Coulibaly Ngolo Issa <coulibalyissa956@gmail.com>
2026-03-08 01:31:36 +0000

    init smsapp app

---
+++ smsapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Mutualist(models.Model):
+    """This class represent a mutualist"""
+    
+    first_name = models.CharField("Prenoms", max_length=100)
+    last_name = models.CharField("Nom", max_length=30)
+    phone_number = models.CharField("Numero", max_length=10)
+
+    class Meta:
+        verbose_name = "Mutualiste"
+        verbose_name_plural = "Mutualistes"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
